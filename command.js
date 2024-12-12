84f077d8a5bacd683bbf5bf59a6e21962cb76586
mobstaz187 <markmark092100@gmail.com>
2024-12-13 07:23:56 +0800

    Add files via upload

---
+++ command.js
@@ -0,0 +1,170 @@
+var youtube = "https://www.youtube.com/watch?v=cpraXaw7dyc";
+var twitter = "https://x.com/_edwardprotocol";
+var contractaddress = "placeholder_for_updating";
+var password = "ribbit";
+var dex = "Transmitting TXN"
+
+transmission = [
+  "<br>",
+  ".... . .-.. .-.. --- / .--- ..- -. ---",
+  ".-- . .-.. -.-. --- -- . / - --- / ... .--. .- -.-. .",
+  "-.-- --- ..- / .- .-. . / -- .- -. -.- .. -. -.. .----. ... / .-.. .- ... - / .... --- .--. .",
+  "... .- ...- . / ..- ... .-.-.- .-.-.- .-.-.- .-.-.-",
+  "<br>"
+];
+aboutme = [
+  "<br>",
+  "I am Rocket, I am in a Space Explorer.",
+  "<br>",
+  "I explore to discover more technologies that I've never seen before.",
+  "I AM NOT A RACCOON FOR F*CK'S SAKE",
+  "<br>",
+];
+
+dex = [
+  "<br>",
+  'Initial Contact: Dex        <a href="' + dex + '" target="_blank">Updating...' + '</a>',
+  "<br>"
+];
+ca = [
+    "<br>",
+    '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">Updating....</a>',
+    "<br>"
+  ];
+ribbit = [
+  "<br>",
+  "$EDWARD",
+  '𝕏        <a href="' + twitter + '" target="_blank">twitter/_edwardprotocol' + '</a>',
+  "contract address (Click to Copy): ticker $EDWARD",
+  '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">Updating....</a>',
+  "<br>"
+];
+social = [
+  "<br>",
+  '𝕏        <a href="' + twitter + '" target="_blank">https://x.com/_edwardprotocol' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline or confidential.",
+  "<br>"
+];
+
+search = [
+'<span id="serts"></span>',
+];
+
+launch = [
+  "<br>",
+  '<span class="command2">Private User:</span> <span class="index">Give me an exploded view.</span>',
+  '<span class="color2">Edward:</span> <span class="index">The compression in cylinder three appears to be low.</span>',
+  '<span class="command2">Private User:</span> <span class="index">Log that.</span>',
+  '<span class="color2">Edward:</span> <span class="index">Right, go ribbit ahead.</span>',
+  '<span class="command2">Private User:</span> <span class="index">Panel 3.</span>',
+  '<h1 class="index">***Machines whirring****</h1>',
+  "<br>"
+];
+
+earthorbit = [
+  "<br>",
+  '<span class="command2">Private User:</span> <span class="index">EDWARD, you up?</span>',
+  '<span class="color2">Edward:</span> <span class="index">For you, sir, always.</span>',
+  '<span class="command2">Private User:</span> <span class="index">I\'d like to open a new project file..</span>',
+  '<span class="color2">Edward:</span> <span class="index">Shall I store this on the Central Database?</span>',
+  '<span class="command2">Private User:</span> <span class="index">Actually, I don\'t know who to trust right now. Till further notice, why don\'t we just keep everything on my private server?</span>',
+  '<span class="color2">Edward:</span> <span class="index">Working on a secret project, are we, sir?</span>',
+  '<span class="command2">Private User:</span> <span class="index">I don\'t want this winding up in the wrong hands. Maybe in mine, it can actually do some good.</span>',
+  "<br>",
+];
+
+logs = [
+  '<h1 class="index">Declassifying mission log.</h1>',
+  '<span class="color2">Day 1:</span> <span class="index">System Booting***</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Hardware Check in Progress</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Core intact.</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Southbridge Intact</span>',
+  '<span class="index">***Machine Whirring***</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Directives Complete</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Mainframe intact</span>',
+  '<span class="color2">Day 3:</span> <span class="index">Memory Check</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Booting Sequence Done</span>',
+  "<br>",
+];
+
+journal = [
+  "<br>",
+  '<span class="color2">Edward Flight:</span> <span class="index">First Part of the Mission</span>',
+  '<span class="color2">Edward in Orbit:</span> <span class="index">Anomalies in Orbit</span>',
+  '<span class="color2">Comm-Tech:</span> <span class="index">Lost Transmission</span>',
+  '<span class="color2">Technology:</span> <span class="index">Blockchain Discovery</span>',
+  '<span class="color2">Exploration:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+help = [
+  '<h1 class="command2">logs (./script.sh)</h1>',
+  '<div class="command1"> privateuser_edwardlog.sh </div></br>',
+  '<div class="command1"> edwardlog_secret.sh </div></br>',
+  '<div class="command1"> edward_selfawareness.sh </div></br>',
+  "<br>",
+  '<span class="command">encryption</span>     Show encrypted message',
+  '<span class="command">raccoon</span>        Who is Edward',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">CA</span>             Contract Address',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">home</span>           Display the Homescreen',
+  '<span class="command">help</span>           Show available Commands',
+  '<span class="command">logs</span>           View Edward Logs',
+  "<br>",
+];
+
+date = [
+  '<p id="date1"></p>',
+];
+
+banner = [
+'<span class="index">Booting....</span>',
+'<span class="index">Checking Hardware</span>',
+'<span class="index">All Components OK!</span>',
+'<br>',
+'<span class="index">Loading Operating System....</span>',
+'<br>',
+'<br>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⠀⣾⠟⠿⢷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⡀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⣿⢸⣷⣄⠙⠻⣿⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⠿⠟⠋⣉⣁⢹⡇⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⢿⡼⣿⣿⣧⡀⠈⢙⣿⡿⠿⠶⢿⣷⣶⣶⣶⣶⣦⣤⣄⡀⢀⣴⣿⠟⠋⠁⣠⣴⣿⣿⡏⢸⡇⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⢸⡇⢿⣿⣿⣿⡄⠉⠀⠀⠀⣰⣿⠟⠋⠁⠀⠀⠀⠉⠛⠿⣿⡿⠁⠀⣠⣾⣿⣿⣿⣿⠇⣾⡇⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⠈⢿⣜⣿⡿⠋⠀⠀⠀⠀⠀⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠀⣼⣿⣿⣿⣿⣿⡿⢠⣿⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⠀⣠⣿⢯⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡿⢁⣾⠇⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⣠⣾⡟⠁⣸⣿⣶⣄⠀⠀⢀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⡟⢀⣾⠋⠀⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⢀⣴⣿⠏⢀⣴⣿⣿⣿⣿⣦⠀⡾⠀⠀⠀⣀⣴⣿⣿⣿⣿⣟⢓⣢⡶⠀⠀⠀⠀⠀⠘⢿⣿⡁⠀⠀⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⢀⣴⣿⠟⠁⣰⣿⣿⣿⠟⢀⠙⣿⣾⡇⠀⣠⣾⣿⣿⡿⠿⢿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠈⠻⣷⡄⠀⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⣴⣿⡿⠗⢰⣼⣿⣿⣿⣿⠀⣹⣿⣿⣿⣧⣾⣿⣿⣿⠃⠀⢠⣤⣹⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠙⣿⣆⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⣾⣿⣯⣤⠀⠈⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⠄⠀⠀⠀⠈⢿⣷⡀⠀⠀⠀</span>',
+'<span class="color2">⠙⢿⣿⣄⠀⢈⣙⣛⣋⣥⣿⣿⠿⠛⠉⠁⠀⢭⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣟⣩⠆⠀⠀⠀⠀⠀⠈⢿⣿⣄⠀⠀</span>',
+'<span class="color2">⠀⠀⠻⣿⣦⣿⣿⣶⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⡆⠀</span>',
+'<span class="color2">⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠈⢙⣿⣿⣾⣿⣿⠆</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠙⣿⣿⡉⣭⠀⠀⠀⠀⠀⠀⠀⠀⣦⣀⡀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⠿⠛⠉⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣷⣦⣤⣄⣠⣤⣴⣶⠞⠛⠋⠙⠓⠀⠀⠀⠀⠀⠀⠀⣀⣠⣴⣶⣿⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣯⡛⠛⠛⠋⠁⠀⠀⢀⣀⣤⣶⣮⣥⣴⣶⣶⣿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+'<span class="color2">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⢿⣿⣶⣶⣶⣶⠿⠟⠛⠛⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span>',
+'<br>',
+'</br>',
+'<span class="color2">Security Countermeasures Online...</span>',
+'<br>',
+'<h1 class="command2">Welcome</h1>',
+'<span class="color2">Enter your command below.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
+
+                                                                                            
